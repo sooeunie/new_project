97a2754cfb253734baeb5985256087e576649c38
Sooeunie <sooeun87@gmail.com>
2016-10-16 17:32:19 -0400

    new file names

--- web.py.py
+++ web.py.py
@@ -33,6 +33,6 @@ def results():
 
 app.run(debug=True)
 
-# port = int(os.environ.get("PORT", 5000))
-# app.run(host="0.0.0.0", port=port)
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
 
