05bc89354f766ead6fc604266ea905c27b54d411
KimYoon Rae <YoonKim@KimYoonui-MacBook-Pro.local>
2016-10-16 13:22:31 -0400

    basik python code for the application

---
+++ web_final_project.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+from googlefinance import getQuotes
+app = Flask(__name__)
+import giphypop
+g = giphypop.Giphy()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/results')
+def results():
+    search_output = request.values.get('search')
+    results = g.search(search_output)
+    media = []
+
+    for result in results:
+        media.append(result.media_url)
+
+    return render_template('results.html', media=media)
+
+
+app.run(debug=True)
+
